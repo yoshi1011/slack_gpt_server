62c39237999a93a5a442712d1caee71aafaf8d1f
Yoshinori Tokuno <yoshinori.tokuno@crowdworks.co.jp>
2024-12-19 17:05:26 +0900

    textの種類に関係なくOpenAIに渡すように修正

--- app/models/slack_message.rb
+++ app/models/slack_message.rb
@@ -7,12 +7,11 @@ class SlackMessage
   def exclude_messages
     slack_messages = slack_client.conversations_replies(channel: @channel, ts: @thread_ts).messages
 
-    messages = slack_messages.select { |message| rich_text_message?(message) }
+    messages = slack_messages.select { |message| text_message?(message) }
 
     # スレをまとめる君へのメンションは除外
     messages = messages.reject { |message| message.text.include?("<@U0850P2U5MJ>") }
-    rich_text_sections = messages.map { |message| exclude_rich_text_section(message.dig(:blocks, 0, :elements)) }.flatten
-    texts = rich_text_sections.map { |section| build_text_from_rich_text_section(section).strip }
+    texts = messages.map { |message| message.text }
 
     numberd_join(texts)
   end
@@ -23,31 +22,12 @@ class SlackMessage
     @slack_client ||= Slack::Web::Client.new
   end
 
-  def rich_text_message?(message)
+  def text_message?(message)
     message.blocks? &&
-    message.blocks[0].type == "rich_text" &&
-    has_rich_text_section?(message.dig(:blocks, 0, :elements))
-  end
-
-  def has_rich_text_section?(elements)
-    elements.any? { |element| element.type == "rich_text_section" }
-  end
-
-  def exclude_rich_text_section(elements)
-    elements.filter { |element| element.type == "rich_text_section" }
+    message.blocks[0].type == "rich_text"
   end
 
   def numberd_join(text_array)
     text_array.map.with_index(1) { |text, index| "#{index}. #{text}" }.join("\n")
-  end
-
-  def build_text_from_rich_text_section(rich_text_section)
-    rich_text_section.elements.map do |element|
-      if element.style.nil?
-        element.text
-      else
-        element.style.code ? "`#{element.text}`" : element.text
-      end
-    end.join("")
   end
 end
