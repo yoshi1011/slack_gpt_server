76aeb736bb77062c7585e48be9a059d5d91cc8d8
Yoshinori Tokuno <yoshinori.tokuno@crowdworks.co.jp>
2024-12-19 16:45:54 +0900

    codeタイプのtextも含められるように修正

--- app/models/slack_message.rb
+++ app/models/slack_message.rb
@@ -12,7 +12,7 @@ class SlackMessage
     # スレをまとめる君へのメンションは除外
     messages = messages.reject { |message| message.text.include?("<@U0850P2U5MJ>") }
     rich_text_sections = messages.map { |message| exclude_rich_text_section(message.dig(:blocks, 0, :elements)) }.flatten
-    texts = rich_text_sections.map { |text| text.dig(:elements, 0, :text).strip }
+    texts = rich_text_sections.map { |section| build_text_from_rich_text_section(section).strip }
 
     numberd_join(texts)
   end
@@ -39,5 +39,15 @@ class SlackMessage
 
   def numberd_join(text_array)
     text_array.map.with_index(1) { |text, index| "#{index}. #{text}" }.join("\n")
+  end
+
+  def build_text_from_rich_text_section(rich_text_section)
+    rich_text_section.elements.map do |element|
+      if element.style.nil?
+        element.text
+      else
+        element.style.code ? "`#{element.text}`" : element.text
+      end
+    end.join("")
   end
 end
